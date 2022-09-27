3e88cc7954f07dc229e10384229be1fe813dd176
timon <timonaki@abv.bg>
2022-09-27 20:56:26 +0300

    napredvame napredvame

--- chess.com_main.py
+++ chess.com_main.py
@@ -1,34 +1,40 @@
 import first_page_operations
 import chess_engine
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import multiprocessing
+from selenium.webdriver import ActionChains
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 import re
 import time
 import threading
 
-
 driver = first_page_operations.driver
 time.sleep(2)
+
+moves_to_chesscom_moves_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
+pattern = re.compile(r'piece [wb]. square\-\d\d')
 
 class FigureLocation:
     def __init__(self):
         self.page_source = BeautifulSoup(driver.page_source, 'html.parser')
-        self.pattern = re.compile(r'piece [wb]. square\-\d\d')
         #print(self.page_source)
 
     def all_current_positions(self):
         html_string = ''
         matches_list = []
-        html_list = (self.page_source.find_all(class_=self.pattern))
+        html_list = (self.page_source.find_all(class_=pattern))
         for i in html_list:
             html_string += str(i)
-        matches = self.pattern.findall(html_string)
+        matches = pattern.findall(html_string)
         for i in matches:
             matches_list.append(i)
         return matches_list
 
     def get_pieces_color(self):
-        time.sleep(2)
+        time.sleep(3)
         result = driver.find_element(By.CSS_SELECTOR, 'text[y="99"]').text
         if result == 'a':
             print('You are playing white!')
@@ -37,36 +43,81 @@ class FigureLocation:
             print('You are playing black!')
             return 'b'
 
+def extract_best_move(best_move):
+    print(best_move)
+    best_move_list = best_move.split('.')
+    if best_move_list[2] != '':
+        best_move = best_move_list[2].split(' ')[0]
+        return best_move
 
 
-first_page_operations.start_game.click()
-flag = False
+def find_class(move):
 
-t1 = threading.Thread(target=FigureLocation().get_pieces_color)
-t1.start()
-
-
-while True:
-    current_moves = []
-    if flag == False:
-
-        previous_move = FigureLocation().all_current_positions()
-    time.sleep(0.5)
-    current_move = FigureLocation().all_current_positions()
-    if previous_move != current_move:
-        current_moves = []
-        for i in previous_move:
-            for j in current_move:
-                if j not in previous_move and j not in current_moves:
-                    current_moves.append(j)
-                if i not in current_move and i not in current_moves:
-                    current_moves.append(i)
-
-        print(f'opa {current_moves}')
-        move = chess_engine.EngineConnector(current_moves).send_move()
-        #todo connect the main and the engine
-    flag = True
-    previous_move = current_move
+    pattern2 = re.compile(f'piece [wb]. square-{move}')
+    piece = driver.find_element(By.CLASS_NAME, pattern2)
+    return piece
 
 
+def translate_best_move(best_move):
+    if len(best_move) == 3:
+        best_move = best_move[1:3]
+        best_move = best_move.replace(best_move[0], str(moves_to_chesscom_moves_dict[best_move[0]]))
+        return best_move
+    if len(best_move) == 2:
+        best_move = best_move.replace(best_move[0], str(moves_to_chesscom_moves_dict[best_move[0]]))
+        return best_move
 
+
+def make_move(result, best_move):
+    previous_move = result[1]
+    previous_move = translate_best_move(previous_move)
+    best_move = translate_best_move(extract_best_move(best_move))
+    previous_move = find_class(previous_move)
+    previous_move.click()
+    best_move = find_class(best_move)
+    best_move.click()
+    #obraten translate
+
+def check_for_end():
+    element = WebDriverWait(driver, timeout=100000).until(EC.presence_of_element_located((By.CLASS_NAME, "game-over-modal-content")))
+    if element:
+        return print('GG')
+
+
+def start_new_game():
+    first_page_operations.start_game.click()
+    flag = False
+    #p1 = multiprocessing.Process() for future performance updates
+    t1 = threading.Thread(target=FigureLocation().get_pieces_color)
+    t2 = threading.Thread(target=check_for_end)
+    t1.start()
+    t2.start()
+
+
+    while True:
+        if flag == False:
+
+            previous_move = FigureLocation().all_current_positions()
+        current_move = FigureLocation().all_current_positions()
+        time.sleep(1)
+        if previous_move != current_move:
+            current_moves = []
+            for i in previous_move:
+                for j in current_move:
+                    if j not in previous_move and j not in current_moves:
+                        current_moves.append(j)
+                    if i not in current_move and i not in current_moves:
+                        current_moves.append(i)
+            if current_moves != []:
+                move = chess_engine.EngineConnector(current_moves).send_move()
+                best_move = chess_engine.EngineOperations(chess_engine.driver).find_the_best_move()
+                print(best_move)
+                #todo fix the best move not to be zero at the start alhamdulillah
+                make_move(move, best_move)
+        flag = True
+        previous_move = current_move
+
+
+start_new_game()
+
+#class="game-over-modal-content" - endgame prozorec
--- chess_engine.py
+++ chess_engine.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import threading
+from selenium.webdriver import ActionChains
+#import threading
 from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get('https://woochess.com/en/analysis')
@@ -19,20 +20,23 @@ def translate_move_coordinate(move):
     return [y, x]
 
 def make_coordinate_batch(coordinates):
+    engine = EngineOperations(driver)
+    if coordinates == '':
+        return coordinates
     current_location = list(coordinates[1].values())[0]
     next_move = list(coordinates[0].values())[0]
-    current_location = EngineOperations(driver).find_element(current_location)
-    next_move = EngineOperations(driver).find_element(next_move)
+    current_location = engine.find_element(current_location)
+    next_move = engine.find_element(next_move)
     batch = [current_location, next_move]
     return batch
 
 class EngineOperations:
     def __init__(self, driver):
         self.driver = driver
-        self.page_source = BeautifulSoup(driver.page_source, 'html.parser')
 
     def find_element(self, coordinates):
-        elements = self.page_source.find_all(class_="row-5277c")[
+        source = BeautifulSoup(self.driver.page_source, 'html.parser')
+        elements = source.find_all(class_="row-5277c")[
             int(coordinates[0]) - 1].children
         elements = list(elements)
         element = str(elements[int(coordinates[1]) - 1]).split(';')[0]
@@ -43,10 +47,10 @@ class EngineOperations:
         current_location = ''
         next_move = ''
         batch = ''
+
         if len(coordinates) == 2:
             batch = make_coordinate_batch(coordinates)
         if len(coordinates) == 3:
-            print(coordinates)
             for j in range(1, 3):
                 if list(coordinates[0].values()) == list(coordinates[j].values()):
                     coordinates.pop(j)
@@ -54,19 +58,27 @@ class EngineOperations:
             batch = make_coordinate_batch(coordinates)
 
         if len(coordinates) == 4:
-            current_moves = coordinates[2:5]
-            print(current_moves)
-            next_move = coordinates[0:2]
-            print(next_move)
-            coordinates = [next_move[1], current_moves[1]]
-            batch = make_coordinate_batch(coordinates)
-            #print(coordinates)
-        #driver_element = self.driver.find_element(By.)
-        # for i in elements:
-        #     print(i)
-        #driver.find_element(By.)
+            king_abreviation = ['bK', 'wK']
+            king_moves = []
+            for i in coordinates:
+                if list(i.keys())[0] in king_abreviation:
+                    king_moves.append(i)
+            batch = make_coordinate_batch(king_moves)
+            print(king_moves)
         return batch
 
+    def drag_piece(self, coordinates):
+        previous_square = coordinates[0]
+        next_square = coordinates[1]
+        previous_piece_location = self.driver.find_element(By.CSS_SELECTOR, f'div[data-square="{previous_square}"]')
+        next_piece_location = self.driver.find_element(By.CSS_SELECTOR, f'div[data-square="{next_square}"]')
+        ActionChains(self.driver).drag_and_drop(previous_piece_location, next_piece_location).perform()
+
+    def find_the_best_move(self):
+        element = self.driver.find_element(By.CSS_SELECTOR, 'p[id="result-line"]')
+        if element.text[0:5] == '+1000':
+            return 'Mate in 1!'
+        return element.text
 
 
 
@@ -75,6 +87,7 @@ class EngineConnector:
         self.move_info = move_info
 
     def send_move(self):
+        engine = EngineOperations(driver)
         result = ''
         full_translated_moves = []
         for i in self.move_info:
@@ -83,13 +96,17 @@ class EngineConnector:
             full_translated_moves.append({piece_name: piece_coordinate})
 
         result = EngineOperations(driver).click_on_element(full_translated_moves)
+        if result == '':
+            return []
+        print(result)
+        engine.drag_piece(result)
+        return result
 
-        return print(result)
-
-    def get_move(self):
-        return self.move_info
 
 
-EngineConnector(['piece br square-68', 'piece br square-88', 'piece bk square-78', 'piece bk square-58']).send_move()
+
+
+
+#EngineConnector(['piece br square-68', 'piece br square-88', 'piece bk square-78', 'piece bk square-58']).send_move()
 #['piece br square-68', 'piece br square-88', 'piece bk square-78', 'piece bk square-58'] - black
-
+#['piece wr square-61', 'piece wk square-71', 'piece wr square-81', 'piece wk square-51'] white
